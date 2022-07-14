c24c49715f0492a19e946b164058e7b0095e3d7e
Raumo0 <dharapko@gmail.com>
2022-07-14 19:09:07 +0200

    Added scheduled re-up for watchers

---
+++ src/main/java/com/mapofzones/tokenmatcher/Constants.java
@@ -0,0 +1,59 @@
+package com.mapofzones.tokenmatcher;
+
+import java.util.ArrayList;
+
+public interface Constants {
+    ArrayList<String> watchersLabels = new ArrayList<>() {{
+        add("akashnet-watcher-app");
+//        add("alteredcarbon-watcher-app");
+        add("assetmantle-watcher-app");
+        add("axelar-watcher-app");
+        add("band-watcher-app");
+        add("bitcanna-watcher-app");
+        add("bitsong-watcher-app");
+        add("certik-watcher-app");
+        add("cheqd-watcher-app");
+        add("chihuahua-watcher-app");
+        add("comdex-watcher-app");
+        add("cosmoshub-watcher-app");
+        add("crescent-watcher-app");
+        add("cronos-watcher-app");
+        add("crypto-org-watcher-app");
+        add("cyber-watcher-app");
+        add("decentr-watcher-app");
+        add("desmos-watcher-app");
+        add("dig-watcher-app");
+        add("emoney-watcher-app");
+        add("evmos-watcher-app");
+        add("fetchai-watcher-app");
+        add("gravitybridge-watcher-app");
+        add("impacthub-watcher-app");
+//        add("injectivecleared-watcher-app");
+//        add("injective-watcher-app");
+        add("irishub-watcher-app");
+        add("juno-watcher-app");
+        add("kava-watcher-app");
+//        add("kichainlogged-watcher-app");
+//        add("kichain-watcher-app");
+        add("konstellation-watcher-app");
+        add("kujira-watcher-app");
+        add("likecoin-watcher-app");
+        add("lum-watcher-app");
+        add("microtick-watcher-app");
+        add("omniflix-watcher-app");
+        add("osmosis-watcher-app");
+        add("panacea-watcher-app");
+        add("persistence-watcher-app");
+        add("regen-watcher-app");
+        add("rizon-watcher-app");
+        add("secret-watcher-app");
+        add("sentinel-watcher-app");
+        add("sifchain-watcher-app");
+        add("sommelier-watcher-app");
+        add("stargaze-watcher-app");
+        add("starname-watcher-app");
+        add("terra-watcher-app");
+        add("umee-watcher-app");
+        add("vidulum-watcher-app");
+    }};
+}
--- src/main/java/com/mapofzones/tokenmatcher/Processor.java
+++ src/main/java/com/mapofzones/tokenmatcher/Processor.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -15,25 +18,59 @@ public class Processor {
     }
 
     public void doScript() {
-        log.info("Starting...");
-        String[] arguments = new String[] {"/bin/sh", "-c", "kubectl -n prod delete pod `kubectl -n prod get pods | awk '$3 == \"CrashLoopBackOff\" {print $1}'`"};
-//        String[] arguments = new String[] {"/bin/sh", "-c", "kubectl -n prod get pods | awk '$3 == \"CrashLoopBackOff\" {print $1}'"};
-//        String[] arguments = new String[] {"kubectl", "get", "namespaces"};
-        try {
-            Process proc = new ProcessBuilder(arguments).start();
-            Thread.sleep(30000L);//30 sec
-            printResults(proc);
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+        log.info("----------Starting iteration...");
+        final String bashLocation = "/bin/sh";
+        final String bashOption = "-c";
+        String cleanupCrashLoopBackOffPodsCommand = "kubectl -n prod delete pod `kubectl -n prod get pods | awk '$3 == \"CrashLoopBackOff\" {print $1}'`";
+        String deletePodByLabelCommandPattern = "kubectl -n prod delete pods -l app=<app_name>";
+
+        for (String label : Constants.watchersLabels) {
+            log.info("Starting sub-loop iteration...");
+            try {
+                log.info("Started CrashLoopBackOff cleanup");
+                ArrayList<String> cleanupCrashLoopBackOffPods = prepareCommand(bashLocation, bashOption, cleanupCrashLoopBackOffPodsCommand);
+                List<String> results = executeCommand(cleanupCrashLoopBackOffPods.toArray(new String[0]));
+                logExecutionResults(results);
+                log.info("Finished CrashLoopBackOff cleanup");
+
+                log.info("Started watcher cleanup");
+                ArrayList<String> deletePodByLabel = prepareCommand(bashLocation, bashOption, deletePodByLabelCommandPattern.replace("<app_name>", label));
+                List<String> results2 = executeCommand(deletePodByLabel.toArray(new String[0]));
+                logExecutionResults(results2);
+                log.info("Finished watcher cleanup");
+
+                log.info("Finished sub-loop iteration...");
+                Thread.sleep(30000L);//30 sec
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        log.info("Finished...");
+        log.info("----------Finished iteration...");
     }
 
-    public static void printResults(Process process) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line = "";
-        while ((line = reader.readLine()) != null) {
-            log.info(line);
+    private ArrayList<String> prepareCommand(String bashLocation, String bashOption, String deletePodByLabelCommand) {
+        return new ArrayList<>() {
+            {
+                add(bashLocation);
+                add(bashOption);
+                add(deletePodByLabelCommand);
+            }
+        };
+    }
+
+    private void logExecutionResults(List<String> results) {
+        for (String result : results) {
+            log.info(result);
         }
+    }
+
+    private List<String> executeCommand(String[] arguments) throws IOException {
+        Process proc = new ProcessBuilder(arguments).start();
+        return getExecutionResults(proc);
+    }
+
+    public static List<String> getExecutionResults(Process process) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        return reader.lines().collect(Collectors.toList());
     }
 }
